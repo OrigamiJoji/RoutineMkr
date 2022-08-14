2e54e30c4d250e9b709aa7443e953b1c9c53d433
OrigamiJoji <georgesaracc@gmail.com>
2022-08-13 22:22:38 -0700

    added images

---
+++ client/src/pages/Profile.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+function Profile() {
+    return (
+        <div>
+            here is your profile
+        </div>
+    )
+}
+
+export default Profile;
