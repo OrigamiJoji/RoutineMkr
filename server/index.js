1211eaf323d25a3b6d86d7796121e0e0bccfb391
OrigamiJoji <georgesaracc@gmail.com>
2022-08-19 22:15:43 -0700

    basic auth

--- server/index.js
+++ server/index.js
@@ -1,22 +1,46 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const config = require('./config.json');
 
 var app = express();
 const PORT = 3001;
 
-mongoose.connect('mongodb://localhost:27017/app');
+mongoose.connect(config.mongodb);
 const connection = mongoose.connection;
 connection.once('open', function() {
     console.log("MongoDB connection established successfully");
 })
+const userSchema = new Schema({
+    email: String,
+    password: String,
+    goals: [{name: String, note: String}],
+    tasks: [{name: String, note: String}]
+})
+const users = mongoose.model('User', userSchema);
 
 console.log('Server hosted on port ' + PORT);
 app.listen(PORT);
 
-app.use(express.static('../client/build'));
-
-app.get('/api', (req, res) => {
-    res.status(200).send({
-        joe: 'close enough time'
+app.get('/api/login/:mail/:pword', (req, res) => {
+    const { mail, pword } = req.params;
+    users.findOne({email: mail}).exec().then((f)=> {
+        const { email, password } = f;
+        if(password === pword) {
+            res.status(200).send("logged in");
+        }
+        else {
+            res.status(200).send("not logged in");
+        }
+        
     });
+});
+app.post('/api/signup/:email/:password', (req, res) => {
+    const { email, password } = req.params;
+    users.insertMany({
+        email: email, 
+        password: password,
+        tasks: [],
+        goals: []});
+    res.status(200);
 });
